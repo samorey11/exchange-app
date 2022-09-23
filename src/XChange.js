2e5f74a053b8bac651288c387063afdc1b38a349
Sam Morey <samorey@outlook.com>
2022-09-22 20:15:57 -0400

    Chart not updating correctly

--- src/XChange.js
+++ src/XChange.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from "react-router-dom";
 import { json, checkStatus } from './utils';
 import arrow from './arrow.jpg';
+import Chart from 'chart.js/auto';
 
 class CurrencyConverter extends React.Component {
   constructor(props) {
@@ -12,17 +13,23 @@ class CurrencyConverter extends React.Component {
       convert: 'AUD',
       currencies: [],
       total: '',
-      rate: '0'
+      rate: '1'
     };
 
     this.handleSubmit = this.handleSubmit.bind(this);
     this.handleBaseChange = this.handleBaseChange.bind(this);
     this.handleAmountChange = this.handleAmountChange.bind(this);
     this.handleConvertChange = this.handleConvertChange.bind(this);
+
+    this.chartRef = React.createRef();
   
   }
 
   componentDidMount() {
+    const { handleBaseChange, handleConvertChange } = this.state;
+    this.convertRate(handleBaseChange, handleConvertChange);
+    this.getHistoricalRates(handleBaseChange, handleConvertChange);
+    
     fetch(`https://altexchangerateapi.herokuapp.com/currencies`)
     .then(checkStatus)
     .then(json)
@@ -39,7 +46,10 @@ class CurrencyConverter extends React.Component {
   }
 
   handleBaseChange(event) {
-    this.setState({ base: event.target.value }); 
+    const base = event.target.value;
+    this.setState({ base }); 
+    this.convertRate(base, this.state.convert);
+    this.getHistoricalRates(base, this.state.convert);
   }
 
   handleAmountChange(event) {
@@ -47,7 +57,10 @@ class CurrencyConverter extends React.Component {
   }
 
   handleConvertChange(event) {
-    this.setState({ convert: event.target.value });
+    const convert = event.target.value;
+    this.setState({ convert });
+    this.convertRate(convert, this.state.base);
+    this.getHistoricalRates(convert, this.state.base);
   }
 
 
@@ -82,7 +95,7 @@ class CurrencyConverter extends React.Component {
     .then(json)
     .then((data) => {
       this.setState({
-        convert: parseFloat(data.rates["${convert}"].rate)
+        quote: parseFloat(data.rates[`${convert}`].rate)
       })
     }).catch((error) => {
       console.log(error);
@@ -97,66 +110,122 @@ class CurrencyConverter extends React.Component {
     });
   }
   
+  getHistoricalRates = () => {
+    const { base, convert } = this.state;
 
+    console.log(`${base}`);
+    console.log(`${convert}`);
+    
+    const endDate = new Date().toISOString().split('T')[0];
+    const startDate = new Date((new Date).getTime() - (30 * 24 * 60 * 60 * 1000)).toISOString().split('T')[0];
+
+    fetch(`https://api.frankfurter.app/${startDate}..${endDate}?from=${base}&to=${convert}`)
+      .then(checkStatus)
+      .then(json)
+      .then(data => {
+        if (data.error) {
+          throw new Error(data.error);
+        }
+
+        const chartLabels = Object.keys(data.rates);
+        const chartData = Object.values(data.rates).map(rate => rate[convert]);
+        const chartLabel = `${base}/${convert}`;
+        this.buildChart(chartLabels, chartData, chartLabel);
+      })
+    .catch(error => console.error(error.message));
+  }
+
+  buildChart = (labels, data, label) => {
+    const chartRef = this.chartRef.current.getContext("2d");
+
+    if (typeof this.chart !== "undefined") {
+      this.chart.destroy();
+    }
+
+    this.chart = new Chart(this.chartRef.current.getContext("2d"), {
+      type: 'line',
+      data: {
+        labels,
+        datasets: [
+          {
+            label: label,
+            data,
+            fill: false,
+            tension: 0,
+          }
+        ]
+      },
+      options: {
+        responsive: true,
+      }
+    })
+
+
+  }
 
   render () {
     const { currencies, base, amount, rate, convert } = this.state;
-    console.log(rate);
+    console.log(`${rate}`);
     
 
     return (
-      <div className="row">
-        <div className="col-4 my-3 ms-5 text-center" id="coin1">
-          <div className="row text-center my-3 mx-auto">
-            <span>Base Currency:</span>
-          </div>
-          <form onChange={this.handleSubmit}>
-            <div className="text-center mx-auto"style={{width: '50%'}}>
-              <select name="base" value={base} onChange={this.handleBaseChange} className="form-control select-menu text-center">
-                {this.state.currencies.map(currency => (
-                  <option key={currency.symbol} value={currency.symbol}>
-                  {currency.symbol} - {currency.name}
-                  </option>
-                ))}
-              </select>
+      <React.Fragment>
+        <div className="container">
+          <div className="row">
+            <div className="col-4 my-3 ms-5 text-center" id="coin1">
+              <div className="row text-center my-3 mx-auto">
+                <span>Base Currency:</span>
+              </div>
+              <form onChange={this.handleSubmit}>
+                <div className="text-center mx-auto"style={{width: '50%'}}>
+                  <select name="base" value={base} onChange={this.handleBaseChange} className="form-control select-menu text-center">
+                    {this.state.currencies.map(currency => (
+                      <option key={currency.symbol} value={currency.symbol}>
+                      {currency.symbol} - {currency.name}
+                      </option>
+                    ))}
+                  </select>
+                </div>
+                <div className="my-3 mx-auto text-center">
+                  <label>
+                    Amount:
+                    <input style={{width: '50%'}} type="number" name="amount" value={amount} onChange={this.handleAmountChange} className="my-3 mx-auto form-control" />
+                  </label>
+                  
+                </div>
+                
+              </form>
             </div>
-            <div className="my-3 mx-auto text-center">
-              <label>
-                Amount:
-                <input style={{width: '50%'}} type="number" name="amount" value={amount} onChange={this.handleAmountChange} className="my-3 mx-auto form-control" />
-              </label>
-              
+            <div className="col-3 my-auto text-center mx-auto">
+              <div className="container-fluid">
+                <img src={arrow} />
+              </div>
             </div>
-            
-          </form>
+            <div className="col-4 my-3 mx-auto text-center" id="coin2">
+              <div className="row text-center my-3 mx-auto">
+                <span>Convert to: </span>
+              </div>
+              <div className="text-center mx-auto" style={{width: '50%'}}>
+                <form onChange={this.handleSubmit}>
+                  <select name="convert" value={convert} onChange={this.handleConvertChange} className="form-control select-menu text-center">
+                    {this.state.currencies.map(currency => (
+                      <option key={currency.symbol} value={currency.symbol}>
+                      {currency.symbol} - {currency.name}
+                    </option>
+                    ))}
+                  </select>
+                </form>
+              </div>
+              <div className="my-3 mx-auto text-center">
+                <span>Total: </span>
+              </div>
+              <span>${amount * rate}</span>
+            </div>
+          </div>
+          <canvas ref={this.chartRef} />
         </div>
-        <div className="col-3 my-auto text-center mx-auto">
-          <div className="container-fluid">
-            <img src={arrow} />
-          </div>
-        </div>
-        <div className="col-4 my-3 mx-auto text-center" id="coin2">
-          <div className="row text-center my-3 mx-auto">
-            <span>Convert to: </span>
-          </div>
-          <div className="text-center mx-auto" style={{width: '50%'}}>
-            <form onChange={this.handleSubmit}>
-              <select name="convert" value={convert} onChange={this.handleConvertChange} className="form-control select-menu text-center">
-                {this.state.currencies.map(currency => (
-                  <option key={currency.symbol} value={currency.symbol}>
-                  {currency.symbol} - {currency.name}
-                </option>
-                ))}
-              </select>
-            </form>
-          </div>
-          <div className="my-3 mx-auto text-center">
-            <span>Total: </span>
-          </div>
-          <span>${amount * rate}</span>
-        </div>
-      </div>
-      )
+      </React.Fragment>
+    )
   }
 }
 
